349032e4d1e28252762ed4194e1ca6780366b396
Shaun McMurray <gusmcmsh@student.gu.se>
2017-09-28 09:12:38 +0200

    initial line animation, TODO add empty GO

---
+++ Unity/DAVE/Assets/Scripts/LineSegment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineSegment : MonoBehaviour {
+
+	float speed = 3f;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 dir =  target.position - this.transform.localPosition;
+		float distThisFrame = speed * Time.deltaTime;
+		if(dir.magnitude <= distThisFrame){
+			Destroy(gameObject);
+		}else{
+			transform.Translate(dir.normalized * distThisFrame, Space.World);
+		}
+		
+	}
+}
---
+++ Unity/DAVE/Assets/Scripts/SystemBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemBox : MonoBehaviour {
+
+	public GameObject lineSegmentPrefab;
+	float cooldown = 0.4f;
+	float cooldownRemaining = 0;
+	GameObject target;
+	// Use this for initialization
+	void Start () {
+
+		Vector3 v = new Vector3(this.transform.position.x, 0, this.transform.position.z);
+		target = (GameObject)Instantiate(lineSegmentPrefab, v, this.transform.rotation);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		cooldownRemaining -= Time.deltaTime;
+		if(cooldownRemaining <= 0){
+			cooldownRemaining = cooldown;
+			newLifeLine(target);
+		}
+		
+	}
+
+	void newLifeLine(GameObject t){
+
+		GameObject lifeLineGO = (GameObject)Instantiate(lineSegmentPrefab, this.transform.position, this.transform.rotation);
+		LineSegment l = lifeLineGO.GetComponent<LineSegment>();
+		l.target = t.transform;
+	}
+}
