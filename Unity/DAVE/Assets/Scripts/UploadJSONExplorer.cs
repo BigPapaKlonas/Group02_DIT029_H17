177865b884a91bb7c30884e583282cb26f66edae
guslauer <guslauer@student.gu.se>
2017-09-30 15:31:39 +0200

    Diagram uploading button

---
+++ Unity/DAVE/Assets/Scripts/UploadJSONExplorer.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using SFB;
+using System.IO;
+
+[RequireComponent(typeof(Button))]
+public class UploadJSONExplorer : MonoBehaviour, IPointerDownHandler
+{
+    public string Title = "";
+    public string FileName = "";
+    public string Directory = "";
+    public string Extension = "txt";
+    public bool Multiselect = false;
+
+    public string output;
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+    //
+    // WebGL
+    //
+    [DllImport("__Internal")]
+    private static extern void UploadFile(string id);
+
+    public void OnPointerDown(PointerEventData eventData) {
+        UploadFile(gameObject.name);
+    }
+
+    // Called from browser
+    public void OnFileUploaded(string url) {
+        StartCoroutine(OutputRoutine(url));
+    }
+#else
+    //
+    // Standalone platforms & editor
+    //
+    public void OnPointerDown(PointerEventData eventData) { }
+
+    void Start()
+    {
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        var paths = StandaloneFileBrowser.OpenFilePanel(Title, Directory, Extension, Multiselect);
+        if (paths.Length > 0)
+        {
+            StartCoroutine(OutputRoutine(new System.Uri(paths[0]).AbsoluteUri));
+        }
+    }
+#endif
+
+    private IEnumerator OutputRoutine(string url)
+    {
+        Debug.Log("Path: " + url);
+
+        var loader = new WWW(url);
+        yield return loader;
+
+        // Read the json from the file into a string
+        output = loader.text;
+    
+       Debug.Log("JSON: " + output);
+
+        /*
+        // Pass the json to JsonUtility, and tell it to create a GameData object from it
+               GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+        public class PlayerInfo
+    {
+        public string name;
+        public int lives;
+        public float health;
+
+        public static PlayerInfo CreateFromJSON(string jsonString)
+        {
+            return JsonUtility.FromJson<PlayerInfo>(jsonString);
+        }
+
+        // Given JSON input:
+        // {"name":"Dr Charles","lives":3,"health":0.8}
+        // this example will return a PlayerInfo object with
+        // name == "Dr Charles", lives == 3, and health == 0.8f.
+    }**/
+
+    }
+}
