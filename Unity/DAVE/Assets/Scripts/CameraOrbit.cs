5aa527dc5939ee43abdc1680d0cc44ca5b99ca7b
justasAtGU <gusstirju@student.gu.se>
2017-10-02 16:11:48 +0200

    added camera drag to x,y axis

--- Unity/DAVE/Assets/Scripts/CameraOrbit.cs
+++ Unity/DAVE/Assets/Scripts/CameraOrbit.cs
@@ -7,14 +7,19 @@ public class CameraOrbit : MonoBehaviour
     private Transform cameraParent;     // The object the camera is being rotated about
 
     private Vector3 cameraRotation;     // Store camera rotation frames
+    private Vector3 oldPosition;         // CameraParent's position before draging
+    private Vector3 newPosition;        //
+
     private float cameraDistance = 10f; // Initial Distance from Camera
+    private float mouseSpeed = 4f;      // Camera Rotation speed
+    private float scrollSpeed = 2f;     // Camera Zoom speed
+    private float rotateDampening = 10f;// Used for a smoother rotation
+    private float scrollDampening = 6f; // USed for a smoother zoom
 
-    public float mouseSpeed = 4f;       // Camera Rotation speed
-    public float scrollSpeed = 2f;      // Camera Zoom speed
-    public float rotateDampening = 10f; // Used for a smoother rotation
-    public float scrollDampening = 6f;  // USed for a smoother zoom
+    bool dragging = false;
+    Plane movePlane;
 
-    public bool rightClicked = false;   // Used to check if RMB (Right Mouse Button) has been pressed down
+    private bool rightClicked = false;   // Used to check if RMB (Right Mouse Button) has been pressed down
 
     // Initialization of transform objects
     void Start()
@@ -26,31 +31,20 @@ public class CameraOrbit : MonoBehaviour
     // LateUpdate() is used to render camera last to avoid rotation/render issues
     void LateUpdate()
     {
-        // If mouse scroll wheel has been moved call method ZoomCamera()
+        // Call method to move camera
+        MoveCamera();
+
+        // Call method to rotate camera
+        RotateCamera();
+
+        // If mouse scroll wheel has been moved call method to zoom camera
         if (Input.GetAxis("Mouse ScrollWheel") != 0f)
         {
             ZoomCamera();
         }
 
-        // If RMB has been pressed set rightCicked to true
-        if (Input.GetMouseButtonDown(1))
-        {
-            rightClicked = true;
-        }
-        // If RMB has been released set rightCicked to false
-        else if (Input.GetMouseButtonUp(1))
-        {
-            rightClicked = false;
-        }
-
-        // Rotate only if RMD is pressed down
-        if (rightClicked)
-        {
-            RotateCamera();   
-        }
-
-        //Actual Camera Rig Transformations
-        Quaternion QT = Quaternion.Euler(cameraRotation.y, cameraRotation.x, 0);
+        //Actual Camera Transformations
+        Quaternion QT = Quaternion.Euler(cameraRotation.y, cameraRotation.x, 0); // Rotates y amount of degrees along x-axis and vise versa
 
         // Animates the rotation
         this.cameraParent.rotation = Quaternion.Lerp(this.cameraParent.rotation, QT, Time.deltaTime * rotateDampening);
@@ -63,19 +57,40 @@ public class CameraOrbit : MonoBehaviour
         }
     }
 
+    void OnMouseDown()
+    {
+        dragging = true;
+        movePlane = new Plane(-Camera.main.transform.forward, transform.position);
+    }
+
     void RotateCamera()
     {
-        //Rotation of the Camera based on Mouse Coordinates
-        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        // If RMB has been pressed set rightCicked to true
+        if (Input.GetMouseButtonDown(1))
         {
-            cameraRotation.x += Input.GetAxis("Mouse X") * mouseSpeed;  // Change camera's x coordinate
-            cameraRotation.y += Input.GetAxis("Mouse Y") * mouseSpeed;  // Change camera's y coordinate
+            rightClicked = true;
+        }
+        // If RMB has been released set rightCicked to false
+        else if (Input.GetMouseButtonUp(1))
+        {
+            rightClicked = false;
+        }
 
-            //Clamps the camera's y coordinate so the camera does not flip or go over the horizon
-            if (cameraRotation.y < 0f)
-                cameraRotation.y = 0f;
-            else if (cameraRotation.y > 90f)
-                cameraRotation.y = 90f;
+        // If RMD is pressed down rotate camera
+        if (rightClicked)
+        {
+            //Rotation of the Camera based on Mouse Coordinates
+            if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+            {
+                cameraRotation.x += Input.GetAxis("Mouse X") * mouseSpeed;  // Change camera's x coordinate
+                cameraRotation.y += Input.GetAxis("Mouse Y") * mouseSpeed;  // Change camera's y coordinate
+
+                //Clamps the camera's y coordinate so the camera does not flip or go over the horizon while rotating
+                if (cameraRotation.y < 0f)
+                    cameraRotation.y = 0f;
+                else if (cameraRotation.y > 90f)
+                    cameraRotation.y = 90f;
+            }
         }
     }
 
@@ -86,5 +101,22 @@ public class CameraOrbit : MonoBehaviour
 
         this.cameraDistance += scrollLength * -1f;                              // Moves the camera when zooming
         this.cameraDistance = Mathf.Clamp(this.cameraDistance, 1.5f, 100f);     // Sets min and max bounds for zoom length
+    }
+
+    void MoveCamera()
+    {
+        // If LMB has been pressed down save the camera parent's locations
+        if (Input.GetMouseButtonDown(0))
+        {
+            oldPosition = cameraParent.position;                                    // oldPosition initialized to camera parent's location when LMB clicked
+            newPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);   // Getting the position of where the parent was moved
+        }
+
+        // Moves the camera parent object
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 currentPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition) - newPosition; // Get the difference between where the mouse clicked and where it moved
+            cameraParent.position = oldPosition - currentPosition * (mouseSpeed * 1.5f);                    // Moving the camera parent's position by reinitializing cameraParent's position
+        }
     }
 }
