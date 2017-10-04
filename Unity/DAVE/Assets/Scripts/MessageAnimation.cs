15c234c43f73b9621557e78c31f19d88e19d1d1f
shaunmcmurray <shaunmcmurray@gmail.com>
2017-10-04 12:17:59 +0200

    initial animation using lines

---
+++ Unity/DAVE/Assets/Scripts/MessageAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageAnimation : MonoBehaviour {
+
+    private LineRenderer messageLine;
+    private float counter;
+    private float dist;
+    public Transform origin;
+    public Transform destination;
+    public float speed  = 50f;
+
+    public TextMesh textMesh;
+
+    // Use this for initialization
+    void Start () {
+
+        messageLine = GetComponent<LineRenderer>();
+        messageLine.SetPosition(0, origin.position);
+        messageLine.SetWidth(.1f, .1f);
+        dist = Vector3.Distance(origin.position, destination.position);
+
+        textMesh = messageLine.GetComponent(typeof(TextMesh)) as TextMesh;
+        textMesh.text = "TEST";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(counter < dist) {
+
+            counter += .1f / speed;
+            
+            
+            Vector3 distThisFrame = Vector3.Lerp(origin.position, destination.position, counter); 
+            messageLine.SetPosition(1, distThisFrame);
+            textMesh.transform.position = distThisFrame;
+        }
+	}
+}
