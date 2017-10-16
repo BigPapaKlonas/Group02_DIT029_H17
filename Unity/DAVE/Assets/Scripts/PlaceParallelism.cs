3c8d89538f1bf78ff3d7af42c8b902be6f6daf96
justasAtGU <gusstirju@student.gu.se>
2017-10-16 15:16:06 +0200

    added tiny script that instantiates a parallel box

--- Unity/DAVE/Assets/Scripts/PlaceParallelism.cs
+++ Unity/DAVE/Assets/Scripts/PlaceParallelism.cs
@@ -38,29 +38,19 @@ public class PlaceParallelism : MonoBehaviour {
         myInstance.zPosition = parallelBox.position.z;
 
         ScaleObject myScale = parallelBox.GetComponent<ScaleObject>();
-        myScale.xScale = parallelBox.localScale.x + 1;
-        myScale.yScale = parallelBox.localScale.y + 1;
-        myScale.zScale = parallelBox.localScale.z + 1;
+        myScale.xScale = parallelBox.localScale.x + 1.5f;
+        myScale.yScale = parallelBox.localScale.y + 1.5f;
+        myScale.zScale = parallelBox.localScale.z + 1.5f;
     }
 
     void FindMax(GameObject[] gameObjectArray)
     {
         foreach (GameObject obj in gameObjectArray)
         {
-            if (obj.transform.position.x > xMax)
-            {
-                xMax = obj.transform.position.x;
-            }
-            if (obj.transform.position.y > yMax)
-            {
-                yMax = obj.transform.position.y;
-            }
-            if (obj.transform.position.z > zMax)
-            {
-                zMax = obj.transform.position.z;
-            }
+            xMax = Mathf.Max(obj.transform.position.x, xMax);
+            yMax = Mathf.Max(obj.transform.position.y, yMax);
+            zMax = Mathf.Max(obj.transform.position.z, zMax);
         }
-
         maxObject = new Vector3(xMax, yMax, zMax);
     }
 
@@ -68,18 +58,9 @@ public class PlaceParallelism : MonoBehaviour {
     {
         foreach (GameObject obj in gameObjectArray)
         {
-            if (obj.transform.position.x < xMin)
-            {
-                xMin = obj.transform.position.x;
-            }
-            if (obj.transform.position.y < yMin)
-            {
-                yMin = obj.transform.position.y;
-            }
-            if (obj.transform.position.z < zMin)
-            {
-                zMin = obj.transform.position.z;
-            }
+            xMin = Mathf.Min(obj.transform.position.x, xMin);
+            yMin = Mathf.Min(obj.transform.position.y, yMin);
+            zMin = Mathf.Min(obj.transform.position.z, zMin); 
         }
         minObject = new Vector3(xMin, yMin, zMin);
     }
