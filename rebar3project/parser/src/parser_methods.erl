dbb451fa6301d38c08a0d94b08c75cad53ada4a3
justasAtGU <gusstirju@student.gu.se>
2017-11-07 22:11:17 +0100

    parsing all messages from all ssd's sub contents

--- rebar3project/parser/src/parser_methods.erl
+++ rebar3project/parser/src/parser_methods.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @author Erik
+%%% @author Erik, Justinas
 %%% @doc JSON parser
 %%%-------------------------------------------------------------------
 -module(parser_methods).
@@ -7,9 +7,8 @@
 
 %% API
 -export([encode/1, parse_to_map/1, get_SD/0, get_DD/0, get_CD/0, get_diagram/1,
-		get_ssd_processes/1, get_class_relationships/1, get_type/1,
-		 get_diagram_contents/1,get_messages_rec/1,
-		get_messages/1, get_classes/1, get_mapping/1, mapsy/1, get_Val/1]).
+		get_ssd_processes/1, get_class_relationships/1, get_type/1, get_diagram_contents/1,
+		get_messages/1, get_classes/1, get_mapping/1, get_Val2/1]).
 
 %% Returns the JSON as an Erlang map without the meta data
 parse_to_map(X) -> Z = remove_meta(decode_map(X)), Z.
@@ -34,22 +33,19 @@ get_diagram(X) -> case get_type(X) of
                     _Else -> 'Error, not a sequence diagram'
                   end.
 
-get_Val(X) -> {ok, Content} = maps:find(<<"content">>, get_diagram(X)),
-  {ok, Messages} = maps:find(<<"content">>, hd(Content)),
-  Val = hd(Messages),
-  maps:to_list(Val).
+get_Val2(X) -> {ok, Content} = maps:find(<<"content">>, get_diagram(X)),
+  get_Val2_r(Content).
 
-% SD = parser_methods:get_SD().
-% parser_methods:mapsy(SD).
+get_Val2_r([]) -> [];
+get_Val2_r([X | Y]) -> {ok, Messages} = maps:find(<<"content">>, X),
+  [parse_content_list(all_maps_to_list(Messages)) | get_Val2_r(Y)].
 
-%% Returns the diagram
-mapsy(X) -> {ok, Content} = maps:find(<<"content">>, get_diagram(X)),
-            {ok, Messages} = maps:find(<<"content">>, hd(Content)),
-            Val = hd(Messages),
-            remove_utf8_encoding(maps:to_list(Val)).
-            %unicode:characters_to_list(Val, utf8).
+parse_content_list([]) -> [];
+parse_content_list([H | T]) -> [remove_utf8_encoding(H) | parse_content_list(T)].
 
-%get_messages_from_list(Key, Map) -> [].
+all_maps_to_list([]) -> [];
+all_maps_to_list([H | T]) -> [maps:to_list(H) | all_maps_to_list(T)].
+
 %% Returns the diagram contents in a list
 get_diagram_contents(X) -> case get_type(X) of
                              <<"sequence_diagram">> -> maps:get(<<"content">>, get_diagram(X));
@@ -59,22 +55,21 @@ get_diagram_contents(X) -> case get_type(X) of
 remove_utf8_encoding([]) -> [];
 remove_utf8_encoding([{F, S} | T]) when is_list(S) ->
   [{unicode:characters_to_list(F, utf8),
-    remove_utf8_encoding(S)} | remove_utf8_encoding(T)];
+    remove_utf8_encoding(S)} |
+    remove_utf8_encoding(T)];
 remove_utf8_encoding([{F, S} | T]) ->
   [{unicode:characters_to_list(F, utf8),
-  unicode:characters_to_list(S, utf8)} | remove_utf8_encoding(T)];
-remove_utf8_encoding([H | T]) -> [unicode:characters_to_list(H, utf8) | remove_utf8_encoding(T)].
+  unicode:characters_to_list(S, utf8)} |
+    remove_utf8_encoding(T)];
+remove_utf8_encoding([H | T]) ->
+  [unicode:characters_to_list(H, utf8) |
+    remove_utf8_encoding(T)].
 
 %% Returns the processes in a list
 get_ssd_processes(X) -> case get_type(X) of
                       <<"diagram">> -> maps:get(<<"processes">>, X);
                       _Else -> 'Error, not a sequence diagram'
                     end.
-
-%% Returns the messages in a list
-get_messages_rec(X) -> proplists:get_all_values(<<"from">>, X).
-
-
 
 %% Returns the messages in a list
 get_messages(X) -> case get_type(X) of
@@ -107,7 +102,7 @@ encode(X) ->
 
 %% Returns a JSON sequence diagram
 get_SD() ->
-  {ok, File} = file:read_file("SD.json"), parse_to_map(File).
+  {ok, File} = file:read_file("triparallel.json"), parse_to_map(File).
 
 %% Returns a JSON class diagram
 get_CD() ->
