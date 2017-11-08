38702501a39687c23a419f73435049cb409ee53d
guslauer <guslauer@student.gu.se>
2017-11-08 23:11:11 +0100

    added a reset scene button

---
+++ Unity/DAVE/Assets/Scripts/ResetSceneBtn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This script simply loads the current scene, thus 'resets' the scene
+/// </summary>
+public class ResetSceneBtn : MonoBehaviour, IPointerDownHandler
+{
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    public void OnPointerDown(PointerEventData eventData) { }
+
+    private void OnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
