35d927f664ee90bbc6118bb8f8b7cb674bd8abb5
guslauer <guslauer@student.gu.se>
2017-11-09 22:39:44 +0100

    Refactored the code based on Justinas' feedback

--- Unity/DAVE/Assets/Scripts/EventLog.cs
+++ Unity/DAVE/Assets/Scripts/EventLog.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Log to display what is going on in the diagram 'in-game'.
-/// Calling Debug.log("logmsg" + "*"+ placeholder.y + "*" + placeholder.z + "*" + "zzz") 
-/// will add "zzz" as a button to the log and onClick will take camera to placeholder position
-/// Based on: https://gist.github.com/mminer/975374
-/// </summary>
-
+/*
+ * Log to display what is going on in the diagram 'in-game'.
+ * Calling Debug.log("logmsg" + "*"+ placeholder.y + "*" + placeholder.z + "*" + "zzz") 
+ * will add "zzz" as a button to the log and onClick will take camera to placeholder position
+ * Based on: https://gist.github.com/mminer/975374
+ **/
 public class EventLog : MonoBehaviour
 {
     // Structure used for each log item 
     struct Log
     {
         public string message;
+        public Vector3 targetPosition;
         public string stackTrace;
         public LogType type;
     }
@@ -23,14 +23,19 @@ public class EventLog : MonoBehaviour
     Vector2 scrollPosition;                 // Used to place ScrollView
     public bool showLogWindow = true;       // True on start
 
-    Rect windowRect = new Rect(Screen.width * 4 / 5f, 0, Screen.width * 1 / 5f, Screen.height);     // Creates the rectangle for the log
-    GUIContent clearLabel = new GUIContent("Clear", "Clear the contents of the console.");          // Label for clear button
-    CameraOrbit cameraOrbitScript;                                                                  // Allows for repositioning the camera
+    // Creates the rectangle for the log
+    Rect windowRect = new Rect(Screen.width * 0.8f, 0, Screen.width * 0.2f, Screen.height);
+    // Label for clear button
+    GUIContent clearLabel = new GUIContent("Clear", "Clear the contents of the console.");
+    // Allows for repositioning the camera
+    CameraOrbit cameraOrbitScript;
 
-    void OnEnable()
+    void OnEnable() //Called when the object becomes enabled and active.
     {
-        Application.logMessageReceived += HandleLog;                                    // Assigned when EventLog object is enabled
-        cameraOrbitScript = (CameraOrbit)Camera.main.GetComponent(typeof(CameraOrbit)); // Gets the cameraOrbit script
+        // Assigns HandleLog function to handle log messages received
+        Application.logMessageReceived += HandleLog;
+        // Gets the cameraOrbit script
+        cameraOrbitScript = (CameraOrbit)Camera.main.GetComponent(typeof(CameraOrbit));
     }
 
     void OnDisable()
@@ -40,7 +45,7 @@ public class EventLog : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(toggleKey))    //Disable the log if key pressed
+        if (Input.GetKeyDown(toggleKey))    // Disable/enables the log on toggle key pressed
         {
             showLogWindow = !showLogWindow;
         }
@@ -50,60 +55,65 @@ public class EventLog : MonoBehaviour
     {
         if (!showLogWindow)
         {
-            return;
+            return; // Returns if log is disabled
         }
 
-        windowRect = GUILayout.Window(123456, windowRect, LogWindow, "Log"); //Creates a window with LogWindow and title
+        // Creates a window with id 123456 based on windowRect dimensions with LogWindow and title "Log"
+        GUILayout.Window(123456, windowRect, LogWindow, "Log");
     }
 
 
-    /// <summary>
-    /// GUI window that displays the log items
-    /// </summary>
-    /// <param name="windowID">Window ID.</param>
+    // GUI window that houses the log items
     void LogWindow(int windowID)
     {
-        scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+        //Create GUIStyles for the different buttons that will be added to the window
+        GUIStyle logBtnStyle = new GUIStyle(GUI.skin.button)
+        {
+            wordWrap = true,
+            alignment = TextAnchor.UpperLeft
+        };
+        GUIStyle clearBtnStyle = new GUIStyle(GUI.skin.button)
+        {
+            alignment = TextAnchor.MiddleCenter
+        };
+
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition); // Starts the scroll view
 
         // Iterate through the logs.
-        for (int i = 0; i < logs.Count; i++)
+        foreach (Log log in logs)
         {
-            var log = logs[i];
-
-            // Creates Vector3 from MessageText position to where the log item's corresponding MessageText is
-            var y = float.Parse(log.message.Split('*')[1]);
-            var z = float.Parse(log.message.Split('*')[2]);
-            Vector3 messageTextPosition = new Vector3(0, y, z);
-        
-            if (GUILayout.Button(log.message = log.message.Split('*')[3], new GUIStyle(GUI.skin.button) { wordWrap = true, alignment = TextAnchor.UpperLeft })) // Creates button
+            // Creates button that executes the if statement on click
+            if (GUILayout.Button(log.message, logBtnStyle))
             {
-                cameraOrbitScript.SetPosition(messageTextPosition); //OnClick the camera's position is set to where the messageText is
+                // Sets camera position to targetPosition
+                cameraOrbitScript.SetPosition(log.targetPosition);
             }
         }
 
-        GUILayout.EndScrollView();
+        GUILayout.EndScrollView();  // Ends the scroll view
 
 
-        GUILayout.BeginHorizontal();
-
-        if (GUILayout.Button(clearLabel, new GUIStyle(GUI.skin.button) { alignment = TextAnchor.MiddleCenter }))
+        if (GUILayout.Button(clearLabel, clearBtnStyle))
         {
-            logs.Clear();   //Clears the log OnClick
+            logs.Clear();   //Clears the log when the button is clicked
         }
-
-        GUILayout.EndHorizontal();
     }
 
-    /// <summary>
-	/// Records a log from the log callback.
-	/// </summary>
-	void HandleLog(string message, string stackTrace, LogType type)
+
+    // Decodes and records a log from the log callback.
+    void HandleLog(string message, string stackTrace, LogType type)
     {
-        if (type.Equals(LogType.Log) && message.StartsWith("logmsg"))   // Checks type and if message starts with logmsg
+        // Checks type and if message starts with logmsg
+        if (type.Equals(LogType.Log) && message.StartsWith("logmsg"))
         {
-            logs.Add(new Log()
+            message = message.Remove(0, 6);             // Removes 'logmsg'
+            var y = float.Parse(message.Split('*')[1]); // Get coordinates from string array
+            var z = float.Parse(message.Split('*')[2]); // and parses to floats
+
+            logs.Add(new Log()                          // Adds a new Log to the list of logs
             {
-                message = message.Remove(0, 6), // Removes logmsg
+                message = message.Split('*')[3],        // Assigns the Log structure's message 
+                targetPosition = new Vector3(0, y, z)   // Creates and assigns a Vector3 based on y and z
             });
         }
     }
