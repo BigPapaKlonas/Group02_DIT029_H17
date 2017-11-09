6c1717ce94ca2a88d506c2ad5d9ec2d5afed3909
justasAtGU <gusstirju@student.gu.se>
2017-11-09 18:06:10 +0100

    added comments to parser_methods

--- rebar3project/parser/src/parser_methods.erl
+++ rebar3project/parser/src/parser_methods.erl
@@ -6,11 +6,11 @@
 -author("Erik and Justinas").
 
 %% API
--export([encode/1, parse_to_map/1, get_SD/0, get_DD/0, get_CD/0,
-  get_diagram/1, get_processes/1, get_relationships/1, get_type/1, get_parsed_diagram/1,
-  get_classes/1, get_mapping/1, get_messages/1, decode_list/1, parse_to_list/1, get_parsed_diagram_test/1]).
+-export([encode/1, parse_to_map/1, parse_to_list/1, get_SD/0, get_DD/0,
+  get_CD/0, get_type/1, get_diagram/1, get_processes/1, get_relationships/1,
+  get_classes/1, get_mapping/1, get_messages/1, get_parsed_diagram/1]).
 
-%% Returns the JSON as an Erlang map without the meta data
+%% Returns the JSON as an Erlang map
 parse_to_map(X) -> decode_map(X).
 %%io:format("The ~p map has the following keys: ~p~n~n", [get_type(Z), maps:keys(Z)])
 
@@ -21,11 +21,10 @@ decode_map(X) ->
     false -> 'not a valid JSON'
   end.
 
-%% Returns the JSON as an Erlang map without the meta data
+%% Returns the JSON as an Erlang list
 parse_to_list(X) -> decode_list(X).
-%%io:format("The ~p map has the following keys: ~p~n~n", [get_type(Z), maps:keys(Z)])
 
-%% Decodes the JSON file into an Erlang map
+%% Decodes the JSON file into an Erlang list
 decode_list(X) ->
   case jsx:is_json(X) of
     true  -> jsx:decode(X);
@@ -35,95 +34,123 @@ decode_list(X) ->
 %% Returns the diagram type
 get_type(X) -> maps:get(<<"type">>, X).
 
-%% Returns the diagram
+%% Returns the diagram from JSON with SSD data
 get_diagram(X) -> case get_type(X) of
                     <<"sequence_diagram">> -> maps:get(<<"diagram">>, X);
                     _Else -> 'Error, not a sequence diagram'
                   end.
 
-get_parsed_diagram(X) -> Z = parse_to_map(X),
-  case get_type(Z) of
-    <<"sequence_diagram">> -> [get_messages(Z) | [get_processes(Z)]];
-    <<"deployment_diagram">> -> get_mapping(Z);
-    _Else -> 'Diagram type not supported'
-  end.
-
-get_parsed_diagram_test(X) ->
+% Returns parsed data from binary JSON
+get_parsed_diagram(X) ->
+  % Parse binary JSON to map in order to check for what kind of diagram it is
   Z = parse_to_map(X),
+  % Check the type of diagram in JSON
   case get_type(Z) of
     <<"sequence_diagram">> -> [get_messages(Z) | [get_processes(Z)]];
     <<"deployment_diagram">> -> get_mapping(Z);
-    <<"class_diagram">> -> Y = parse_to_list(X), [get_classes(Y) | [get_relationships(Y)]];
+    <<"class_diagram">> -> Y = parse_to_list(X), % Parse CD to list, because it's easier to work with it that way
+      [get_classes(Y) | [get_relationships(Y)]];
     _Else -> 'Diagram type not supported'
   end.
 
-get_messages(X) -> {ok, Content} = maps:find(<<"content">>, get_diagram(X)),
+% Return parsed messages from SSD JSON
+get_messages(X) ->
+  % Find map with the "content" key
+  {ok, Content} = maps:find(<<"content">>, get_diagram(X)),
+  % parse content
   parse_content(Content).
 
-parse_content([]) -> [];
-parse_content([X | Y]) -> {ok, Messages} = maps:find(<<"content">>, X),
-  [parse_list(all_maps_to_list(Messages)) | parse_content(Y)].
-
-parse_classes([]) -> [];
-parse_classes([H | T]) -> [parse_fields(H) | parse_classes(T)].
-
-parse_list([]) -> [];
-parse_list([H | T]) -> [remove_utf8_encoding(H) | parse_list(T)].
-
-all_maps_to_list([]) -> [];
-all_maps_to_list([H | T]) -> [maps:to_list(H) | all_maps_to_list(T)].
-
-remove_utf8_encoding([]) -> [];
-remove_utf8_encoding([{F, S} | T]) when is_list(S) ->
-  [{unicode:characters_to_list(F, utf8),
-    remove_utf8_encoding(S)} |
-    remove_utf8_encoding(T)];
-remove_utf8_encoding([{F, S} | T]) ->
-  [{unicode:characters_to_list(F, utf8),
-  unicode:characters_to_list(S, utf8)} |
-    remove_utf8_encoding(T)];
-remove_utf8_encoding([H | T]) ->
-  [unicode:characters_to_list(H, utf8) |
-    remove_utf8_encoding(T)];
-remove_utf8_encoding({H, T}) ->
-  [{unicode:characters_to_list(H, utf8),
-    unicode:characters_to_list(T, utf8)}].
-
-remove_utf8_class([]) -> [];
-remove_utf8_class([L]) -> remove_utf8_class(L);
-remove_utf8_class([H | T]) ->
-  [remove_utf8_class(H),
-    remove_utf8_class(T)];
-remove_utf8_class({H, T}) ->
-  {unicode:characters_to_list(H, utf8),
-    unicode:characters_to_list(T, utf8)}.
-
-%% Returns the processes in a list
+%% Returns the parsed process from SSD
 get_processes(X) -> case get_type(X) of
-                      <<"sequence_diagram">> -> parse_list(
-                        all_maps_to_list(maps:get(<<"processes">>, X)));
+                      <<"sequence_diagram">> ->
+                        % Remove the binary encoding from the list
+                        parse_list(
+                          % Convert the maps to list
+                          maps_to_list(
+                            % Get the "processes" map from JSON
+                            maps:get(<<"processes">>, X)));
                       _Else -> 'Error, not a sequence diagram'
                     end.
 
-parse_fields([{NameKey, Name}, {Field, List}]) ->
-  [{unicode:characters_to_list(NameKey),
-    unicode:characters_to_list(Name)}, {unicode:characters_to_list(Field),
-    remove_utf8_class(List)}].
-
 %% Returns the classes from CD
-get_classes(X) -> parse_classes(proplists:get_value(<<"classes">>, X)).
+get_classes(X) ->
+  % Remove the binary encoding with a pattern matching function
+  iterate_classes(
+    % Get the list of "classes" from JSON
+    proplists:get_value(<<"classes">>, X)).
 
-%% Returns the relationships in a list
-get_relationships(X) -> parse_list(proplists:get_value(<<"relationships">>, X)).
+%% Returns the class relationships from CD
+get_relationships(X) ->
+  % Remove the binary encoding from the list
+  parse_list(
+    % Get the list of "relationships" from JSON
+    proplists:get_value(<<"relationships">>, X)).
 
 
-%% Returns the diagram type
+%% Returns the device mapping from DD
 get_mapping(X) -> case get_type(X) of
-                    <<"deployment_diagram">> -> parse_list(
-                      all_maps_to_list(maps:get(<<"mapping">>, X)));
+                    <<"deployment_diagram">> ->
+                      % Remove the binary encoding from the list
+                      parse_list(
+                        % Convert the maps to list
+                        maps_to_list(
+                          % Get the "mapping" map from JSON
+                          maps:get(<<"mapping">>, X)));
                     _Else -> 'Error, not a deployment diagram'
                   end.
 
+% Traverses all the different sub-contents of SDD while parsing them
+% Reason for the method to being separate is to find the sub-contents
+parse_content([]) -> [];
+parse_content([X | Y]) ->
+  % Find the sub-contents from JSON, project description says there will be at least one
+  {ok, Messages} = maps:find(<<"content">>, X),
+  % Turn the sub content map into a list and parse it before making a recursive call to the next sub-content
+  [parse_list(maps_to_list(Messages)) | parse_content(Y)].
+
+% Traverses all the different classes in CD and calls a function to parse them with pattern matching
+iterate_classes([]) -> [];
+iterate_classes([H | T]) -> [parse_fields(H) | iterate_classes(T)].
+
+% Parses the sub-fields within CD JSON
+parse_fields([{NameKey, Name}, {Field, FieldList}]) ->
+  % Remove binary utf8 encoding from the values
+  [{unicode:characters_to_list(NameKey), unicode:characters_to_list(Name)},
+    {unicode:characters_to_list(Field),
+      % Call a function to parse the different elements within the Field List
+      remove_utf8_class(FieldList)}].
+
+% Traverses lists and calls a function to remove binary encoding for the Head
+parse_list([]) -> [];
+parse_list([H | T]) -> [remove_utf8(H) | parse_list(T)].
+
+% Changes the top level maps to lists
+maps_to_list([]) -> [];
+maps_to_list([H | T]) -> [maps:to_list(H) | maps_to_list(T)].
+
+% Removes binary utf8 encoding from DD and SSD
+remove_utf8([]) -> [];
+% Removes the utf8 encoding before making a recursive call to the tail
+remove_utf8([{F, S} | T]) ->
+  [{unicode:characters_to_list(F, utf8),
+    unicode:characters_to_list(S, utf8)} |
+    remove_utf8(T)].
+
+% Removes binary utf8 encoding from CD
+remove_utf8_class([]) -> [];
+% The fields come in in 2 nested lists, clause is used to remove the fields(L) from the top layer list
+% This interferes with the SSD and DD diagrams, hence the separate method
+remove_utf8_class([L]) -> remove_utf8_class(L);
+% Clause used to iterate through
+remove_utf8_class([H | T]) ->
+  % Recursive call to parse the Field list
+  [remove_utf8_class(H),
+    % Recursive call to parse the next Field list
+    remove_utf8_class(T)];
+% Clause used to remove utf8 binary encoding
+remove_utf8_class({H, T}) ->
+  {unicode:characters_to_list(H, utf8),
+    unicode:characters_to_list(T, utf8)}.
 
 %% Converts Erlang binary map into JSON string
 encode(X) ->
