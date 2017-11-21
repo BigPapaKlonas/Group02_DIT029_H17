a1a905ff9661734aebcaec83f4eb79de51c98ed2
guslauer <guslauer@student.gu.se>
2017-11-21 11:54:40 +0100

    added comments to python scripts

--- test/container.py
+++ test/container.py
@@ -1,25 +1,20 @@
 import docker
 import json
+import uuid
 
 
-docker_client = docker.from_env()
+docker_client = docker.from_env()   # getting the default socket and configuration in our environment
 
+# Start docker containers according to JSON string (processes)
 def start_containers(processes):
     try:
-        decoded = json.loads(processes)
+        decoded = json.loads(processes)             # Decodes JSON
+        processes_id = uuid.uuid4().hex             # Generates unique ID for containers created from this JSON string
 
-        # Access data
-        for x in decoded:
-            name = x['name']
-            if (len(x['name']) < 2):
-                name = '0' + name
-            print(name)
-            tmp_container = docker_client.containers.run(image='hello-world', detach=True, name = name, auto_remove=True)
-            print(tmp_container.logs())
+        for x in decoded:                                       # Iterates over the decoded JSON
+            container_name = x['name'] + "_" + processes_id     # Adds processes_id to the container name
+            # Starts a container, detaches and names it & removes it from daemon side when the container's process exits
+            docker_client.containers.run(image='hello-world', detach=True, name=container_name, auto_remove=True)
 
-    except (ValueError, KeyError, TypeError):
+    except (ValueError, KeyError, TypeError):   # In case the JSON is either invalid or does not contain 'name'
         print("JSON format error")
-
-
-
-
--- test/mqtt_client.py
+++ test/mqtt_client.py
@@ -5,31 +5,29 @@ import container
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.publish("processes", "python has started!", qos=2)
-    client.subscribe("processes/#", qos=2)
+    client.subscribe("processes/#", qos=2)          # Subscribes to topic with QoS 2
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print("Received: "+str(msg.payload))
 
+    # In case received message is of specified topic, the message is past into the start_containers function
     if(msg.topic == "processes"):
         container.start_containers(msg.payload.decode("utf-8"))
 
-# 'client.loop_forever()' automatically handles reconnecting as long as the initial connection does not fails
-# this function makes sure that the code does not crash in case initial connection does fail
+# 'loop_forever' automatically handles reconnecting as long as the initial connection succeeds (crashes otherwise)
+# The 'connect' function retries to establish the initial connection in case it fails, thus prevents a potential crash
 def connect():
     try:
-	client.connect("localhost", 1883, 60)
-    except socket.error as error:
+        client.connect("localhost", 1883, 60)
+    except socket.error as error:               # Catches socket error and retries to establish the connection
         print(error)
         print("I'll try again...")
         connect()
 
-client = mqtt.Client()
-client.on_connect = on_connect
+
+client = mqtt.Client()          # Creates and assigns the mqtt client
+client.on_connect = on_connect  # Assigns callback to function
 client.on_message = on_message
 connect()
-client.loop_forever();
+client.loop_forever()           # Makes the python script run (awaits for messages) until infinity...
