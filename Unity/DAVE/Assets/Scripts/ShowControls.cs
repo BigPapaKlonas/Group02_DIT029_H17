96c1ef9e77a2ea1a64f3d3f85d582e069af1b517
guslauer <guslauer@student.gu.se>
2017-11-26 20:10:47 +0100

    all scenes are using the same layout and colors

---
+++ Unity/DAVE/Assets/Scripts/ShowControls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class ShowControls : MonoBehaviour {
+
+    private GameObject CameraControlPanel;
+    private Image Arrow;
+    public Sprite downArrow;
+    public Sprite upArrow;
+
+    // Gets objects and sets click listener
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+        CameraControlPanel = GameObject.FindGameObjectWithTag("CameraControlPanel");
+        Arrow = GameObject.FindGameObjectWithTag("arrow").GetComponent<Image>();
+    }
+
+    private void OnClick()
+    {
+        if (CameraControlPanel.activeSelf)          // If active
+        {
+            CameraControlPanel.SetActive(false);    // Deactivates panel
+            Arrow.sprite = upArrow;                 // Make drop down arrow point upwards
+        }
+        else
+        { 
+            Arrow.sprite = downArrow;
+            CameraControlPanel.SetActive(true);
+        }
+    }
+}
