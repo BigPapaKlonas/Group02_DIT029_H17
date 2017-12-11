f071e7badce71ca1b3c6f333abd6b5967fa5a330
guslauer <guslauer@student.gu.se>
2017-12-11 19:07:01 +0100

    Bug fix

--- Unity/DAVE/Assets/Scripts/DiagramDistributor/PublishDiagram.cs
+++ Unity/DAVE/Assets/Scripts/DiagramDistributor/PublishDiagram.cs
@@ -18,7 +18,7 @@ public class PublishDiagram : MonoBehaviour
         button = GetComponent<Button>();
         button.onClick.AddListener(OnClick);
 
-        if (coordinator.GetInstructorBool() == false)
+        if (ConnectionManager.auth == false)
         {
             button.gameObject.SetActive(false);
         }
