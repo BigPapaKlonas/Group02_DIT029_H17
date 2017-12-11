70fba760b2f999d2a429f3fb586f48f5c4a9bdf3
guslauer <guslauer@student.gu.se>
2017-12-11 14:40:37 +0100

    bug fixes

--- Unity/DAVE/Assets/Scripts/DiagramDistributor/PublishDiagram.cs
+++ Unity/DAVE/Assets/Scripts/DiagramDistributor/PublishDiagram.cs
@@ -1,59 +1,64 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class PublishDiagram : MonoBehaviour {
+public class PublishDiagram : MonoBehaviour
+{
 
-    public GameObject ssdSpawnerPrefab;
     public GameObject player;
-
-	private Button button;
+    private Button button;
     ConnectionManager coordinator = ConnectionManager.coordinator;
 
     // Use this for initialization
-    void Start () 
-	{
+    void Start()
+    {
         button = GetComponent<Button>();
-		button.onClick.AddListener(OnClick);
+        button.onClick.AddListener(OnClick);
 
-		if (coordinator.GetInstructorBool () == false) {
-			button.gameObject.SetActive (false);
-		}
-	}
+        if (coordinator.GetInstructorBool() == false)
+        {
+            button.gameObject.SetActive(false);
+        }
+    }
 
-     void OnClick() 
-	{
+    void OnClick()
+    {
         // Iterates through the selected JSONS and publishes them to the MQTT broker
         while (coordinator.GetSelectedJsons().Count > 0)
         {
             var jsonStruct = coordinator.GetSelectedJsons().Dequeue();
 
-            if (jsonStruct.diagramType == "sequence_diagram") {
+            if (jsonStruct.diagramType == "sequence_diagram")
+            {
 
                 string ssdRoom = "root/" + coordinator.GetInstructor() + "/" +
                     coordinator.GetRoom() + "/sequence_diagram";
                 coordinator.Publish(
                     "root/newdiagram",
                     ssdRoom,
-                    true
+                    false
                 );
-                GameObject SSDGO = (GameObject)Instantiate(
-                    ssdSpawnerPrefab,
-                    player.transform.position,
-                    player.transform.rotation
-                );
-                SSDSpawner spawner = SSDGO.GetComponent<SSDSpawner>();
-                spawner.room = ssdRoom;
-                Debug.Log(ssdRoom);
-            } 
+            }
 
-            coordinator.Publish(
+            // If the diagram is a sequence diagram, it is retained for the simulation to work
+            if (jsonStruct.diagramType == "sequence_diagram")
+            {
+                coordinator.Publish(
                 "root/" + coordinator.GetInstructor() + "/" +
                 coordinator.GetRoom() + "/" + jsonStruct.diagramType,
                 jsonStruct.json,
-                true
-            );
+                true);
+            }
+            else
+            {
+                coordinator.Publish(
+                "root/" + coordinator.GetInstructor() + "/" +
+                coordinator.GetRoom() + "/" + jsonStruct.diagramType,
+                jsonStruct.json,
+                false
+                );
+            }
         }
-        
+
         gameObject.SetActive(false);
-	}
+    }
 }
